052dac390abed5925214033978396097beba5fec
BoJeong Ko <118041575+uglysweetpotatos@users.noreply.github.com>
2024-04-05 15:06:00 +0900

    채팅

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,25 +1,22 @@
-import { useState, useEffect } from 'react';
-import Movie from './components/Movie';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import MovieVideo from './routes/MovieVideo';
-import EmptyPage from './components/EmptyPage';
-import Login from './routes/login';
+import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import MovieVideo from "./routes/MovieVideo";
+import EmptyPage from "./components/EmptyPage";
+import Login from "./routes/login";
 
 function MovieApp() {
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    path={`${process.env.PUBLIC_URL}/`}
-                    element={<Login />}
-                />
-                <Route path={`/Movie`} element={<Home />} />
-                <Route path="/MovieVideo/:id" element={<MovieVideo />} />
-                <Route path="*" element={<EmptyPage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Login />} />
+        <Route path={`/Movie`} element={<Home />} />
+        <Route path="/MovieVideo/:id" element={<MovieVideo />} />
+        <Route path="*" element={<EmptyPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default MovieApp;
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,59 +1,20 @@
-import { useEffect, useState, React } from 'react';
+import { useEffect, useState, React } from "react";
+import "../routes/Login.css";
 
 function Login() {
-    const dispatch = useDispatch();
-
-    const [Email, setEmail] = useState('');
-    const [Password, setPassword] = useState('');
-
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
-    };
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    };
-    const onSubmitHandler = (event) => {
-        // 버튼만 누르면 리로드 되는것을 막아줌
-        event.preventDefault();
-
-        console.log('Email', Email);
-        console.log('Password', Password);
-
-        let body = {
-            email: Email,
-            password: Password,
-        };
-
-        dispatch(loginUser(body));
-    };
-
-    return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '100vh',
-            }}
-        >
-            <form
-                style={{ display: 'flex', flexDirection: 'column' }}
-                onSubmit={onSubmitHandler}
-            >
-                <label>Email</label>
-                <input type="email" value={Email} onChange={onEmailHandler} />
-                <label>Password</label>
-                <input
-                    type="password"
-                    value={Password}
-                    onChange={onPasswordHandler}
-                />
-                <br />
-                <button formAction="">Login</button>
-            </form>
-        </div>
-    );
+  const pictureLength = 7; //이미지마지막숫자
+  const randomNumber = Math.floor(Math.random() * pictureLength + 1);
+  console.log(randomNumber);
+  return (
+    <div className="body">
+      <img
+        className="backGroundImg"
+        src={require(`../img/background/${randomNumber}.jpg`)}
+        alt="logo image"
+      />
+      <a href="/movie">영화리스트</a>
+    </div>
+  );
 }
 
 export default Login;
